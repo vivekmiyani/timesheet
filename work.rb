8f1b335d1cb919ceefb4061ff3fee903eb5e5d5f
Vivek <vivekmiyaniofficial@gmail.com>
2025-09-29 18:25:55 +0530

    Migrated to GraphQL

--- work.rb
+++ work.rb
@@ -1,118 +1,162 @@
 #!/usr/bin/env ruby
 
-require "yaml"
-require "octokit"
+# frozen_string_literal: true
+
+require "faraday"
 
 class Work
   attr_reader \
     :date_range,
-    :repo,
     :token
 
-  MERGE_COMMIT = ["Merge", "branch", "into"]
-
-  def initialize(date_range:, repo:, token:)
-    @date_range  = parse_date_range(date_range)
-    @repo        = repo
-    @token       = token
+  def initialize(date_range:, token:)
+    @date_range = parse_date_range(date_range)
+    @token = token
   end
 
-  def merged
-    pulls = client.search_issues("is:merged repo:#{repo} type:pr updated:>=#{date_range.begin}", { per_page: 100 }).items
+  def print_summary
+    search.each do |(repo, title), work|
+      puts "-" * 50, "#{repo}:", "-" * 50
 
-    pulls = pulls.find_all { |i| date_range.cover?(i.pull_request.merged_at.to_datetime) }
+      puts title
 
-    pulls.map { |i| client.pull_request(repo, i.number) }.find_all { |i| i.merged_by.login == username }
-  end
+      work[:commits].each do |i|
+        puts "- #{i}"
+      end
 
-  def reviewed
-    pulls = client.search_issues("-author:#{username} repo:#{repo} reviewed-by:#{username} type:pr updated:>=#{date_range.begin}", { per_page: 100 }).items
+      puts "- Reviewed" if work[:reviewed]
 
-    pulls.select { |p| filter_reviews(p).any? }
-  end
-
-  def worked_on
-    pulls = client.search_issues("repo:#{repo} type:pr updated:>=#{date_range.begin}", { per_page: 100 }).items
-
-    pulls.each_with_object({}) do |pull, hash|
-      commits = filter_commits(pull)
-      hash[pull] = commits if commits.any?
+      puts "- Merged" if work[:merged]
     end
   end
 
   private
 
-  def client
-    @client ||= Octokit::Client.new(access_token: token)
-  end
-
-  def user
-    @user ||= client.user
-  end
-
-  def username
-    user.login
-  end
-
   def parse_date_range(date_range)
-    zone       = DateTime.now.zone
+    zone = DateTime.now.zone
     date_range = date_range.split("..")
 
     from = "#{date_range.first}T00:00:00#{zone}"
-    to   = "#{date_range.last}T23:59:59#{zone}"
+    to = "#{date_range.last}T23:59:59#{zone}"
 
     DateTime.parse(from)..DateTime.parse(to)
   end
 
-  def filter_reviews(pull)
-    reviews = client.pull_request_reviews repo, pull.number, per_page: 100
+  def request(query:, variables:)
+    body = { query: query, variables: variables }.to_json
+    headers = { Authorization: "Bearer #{token}" }
 
-    reviews.select { |r| r.user.login == username && date_range.cover?(r.submitted_at.to_datetime) }
+    body = Faraday.post("https://api.github.com/graphql", body, headers).body
+
+    JSON.parse(body)
   end
 
-  def filter_commits(pull)
-    commits = client.pull_request_commits repo, pull.number, per_page: 250
-    commits = commits.reject { |n| (MERGE_COMMIT - n.commit.message.split).empty? }
+  def search # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    repositories = viewer[:repositories].map { |i| "repo:#{i}" }.join(" ")
 
-    commits = commits.select { |c| c.author&.login == username }
-    commits.select { |c| date_range.cover?(c.commit.author.date.to_datetime) }
+    variables = {
+      query: "type:pr updated:>=#{date_range.begin} #{repositories}",
+      type: "ISSUE"
+    }
+
+    response = request(query: search_query, variables: variables)["data"]["search"]["nodes"]
+
+    response.each_with_object({}) do |node, object|
+      commits = node["commits"]["nodes"].find_all do |commit|
+        commit.dig("commit", "author", "user", "login") == viewer[:login] && date_range.cover?(DateTime.parse(commit["commit"]["committedDate"])) # rubocop:disable Layout/LineLength
+      end
+      commits = commits.map { |i| i["commit"]["message"] }
+
+      reviewed = node["latestReviews"]["nodes"].find_all do |review|
+        review.dig("author", "login") == viewer[:login] && date_range.cover?(DateTime.parse(review["submittedAt"]))
+      end.length.positive?
+
+      merged = node.dig("mergedBy", "login") == viewer[:login] && date_range.cover?(DateTime.parse(node["mergedAt"]))
+
+      key = [node["repository"]["nameWithOwner"], node["title"]]
+
+      if commits.length.positive? || reviewed || merged
+        object[key] = { commits: commits, reviewed: reviewed, merged: merged }
+      end
+    end
+  end
+
+  def search_query
+    <<~GQL
+      query search($query: String!, $type: SearchType!) {
+        search(last: 100, query: $query, type: $type) {
+          nodes {
+            ... on PullRequest {
+              commits(last: 250) {
+                nodes {
+                  commit {
+                    committedDate
+                    message
+                    author {
+                      user {
+                        login
+                      }
+                    }
+                  }
+                }
+              }
+              latestReviews(last: 100) {
+                nodes {
+                  author {
+                    login
+                  }
+                  submittedAt
+                }
+              }
+              mergedAt
+              mergedBy {
+                login
+              }
+              repository {
+                nameWithOwner
+              }
+              title
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def viewer
+    @viewer ||= begin
+      response = request(query: viewer_query, variables: {})["data"]["viewer"]
+
+      {
+        login: response["login"],
+        repositories: response["repositoriesContributedTo"]["nodes"].map { |i| i["nameWithOwner"] }
+      }
+    end
+  end
+
+  def viewer_query
+    <<~GQL
+      query viewer {
+        viewer {
+          login
+          repositoriesContributedTo(
+            first: 100
+            orderBy: { direction: DESC, field: PUSHED_AT }
+            includeUserRepositories: true
+          ) {
+            nodes {
+              nameWithOwner
+            }
+          }
+        }
+      }
+    GQL
   end
 end
 
 date_range = ARGV.fetch(0)
-group_key  = ARGV.fetch(1)
-groups     = YAML.load_file("#{__dir__}/projects.yml").slice(group_key)
+token = File.read("#{ENV['HOME']}/.timesheet-token")
 
-groups.each do |_, projects|
-  projects.each do |project|
-    Octokit.configure do |c|
-      c.api_endpoint = project.fetch("api_endpoint")
-    end
+work = Work.new(date_range: date_range, token: token)
 
-    work = Work.new(date_range: date_range, repo: project.fetch("repo"), token: project.fetch("token"))
-
-    puts  "-" * 50, "#{project.fetch("repo")}:", "-" * 50
-
-    puts "*Reviewed:*"
-
-    work.reviewed.each do |pull|
-      puts "#{pull.title} [(##{pull.number})](#{pull.html_url})"
-    end
-
-    puts "\n*Merged:*"
-
-    work.merged.each do |pull|
-      puts "#{pull.title} [(##{pull.number})](#{pull.html_url})"
-    end
-
-    puts "\n*Worked on:*"
-
-    work.worked_on.each do |pull, commits|
-      puts "#{pull.title} [(##{pull.number})](#{pull.html_url})"
-
-      commits.each do |commit|
-        puts commit.commit.message
-      end
-    end
-  end
-end
+work.print_summary
